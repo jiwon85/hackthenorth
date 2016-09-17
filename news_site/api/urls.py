ee6caab7826cebf556bf8c7f1ece18046d926f66
jiwon85 <jiwon.min85@gmail.com>
2016-09-17 16:36:37 -0500

    fixed urls.py

--- news_site/api/urls.py
+++ news_site/api/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^placeholder/$', views.index, name='index'),
     # api
-    url(r'^api/v1/articles/$', views.get_articles, name='get_articles')
+    url(r'^v1/articles/$', views.get_articles, name='get_articles')
 ]
