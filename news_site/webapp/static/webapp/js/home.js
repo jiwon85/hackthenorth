a082467652244560e683b454b64aaf24a8cf1ac5
jiwon85 <jiwon.min85@gmail.com>
2016-09-17 12:50:22 -0500

    angular and django are friends now

--- news_site/webapp/static/webapp/js/home.js
+++ news_site/webapp/static/webapp/js/home.js
@@ -7,7 +7,13 @@ myApp.controller('HomeController', ['$scope', function($scope) {
   	username: "",
   	password: ""
   };
-  $scope.feed = [{title: "1", snippet: "1"}, {title: "2", snippet: "2"}, {title: "3", snippet: "3"}, {title: "4", snippet: "4"}];
+  var title = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi auctor.";
+  var snippet = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis est nec accumsan auctor. Aliquam sollicitudin mollis mi vel pretium. Praesent tellus risus, pulvinar nec commodo eu, egestas eu purus. Nulla feugiat, dui nec tristique ornare, nunc lectus auctor libero, venenatis tincidunt nunc nibh at elit. Donec eu congue ligula, et tincidunt arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla porta lobortis suscipit. Aliquam sed eros quis enim blandit vehicula. Integer ac nulla non massa vehicula pulvinar. Aenean varius nisi non odio ultricies, sit amet iaculis nisi porta.";
+  $scope.feed = [
+    {title: "1"+title, snippet: "1"+snippet},
+    {title: "2"+title, snippet: "2"+snippet},
+    {title: "3"+title, snippet: "3"+snippet},
+    {title: "4"+title, snippet: "4"+snippet}];
   $scope.count = 5;
 
 
@@ -25,7 +31,7 @@ myApp.controller('HomeController', ['$scope', function($scope) {
     //make api call to load more
     newScope = $scope.count+100;
     while($scope.count <= newScope) {
-    	$scope.feed.push({title:$scope.count, snippet:$scope.count});
+    	$scope.feed.push({title:$scope.count+title, snippet:$scope.count+snippet});
     	$scope.count++;
     }
 
