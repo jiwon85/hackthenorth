01d03246b266cf595d6cfa00f3b297aa1575f6cd
Steven Bairos-Novak <sbairosnovak@gmail.com>
2016-09-17 17:34:33 -0500

    Server is up

--- news_site/api/models.py
+++ news_site/api/models.py
@@ -12,36 +12,6 @@ class ArticleInfo(models.Model):
 	article_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	topic_id = models.ForeignKey('Topic', on_delete=models.CASCADE)
 
-# class CategoryEnum(models.Field):
-
-#     def __init__(self, *args, **kwargs):
-#         super(CategoryEnum, self).__init__(*args, **kwargs)
-#         if not self.choices:
-#             raise AttributeError('EnumField requires `choices` attribute.')
-
-#     def db_type(self):
-#         return "enum(%s)" % ','.join("'%s'" % k for (k, _) in self.choices)
-
-# CATEGORY_POLITICS = 0
-# CATEGORY_SPORTS = 1
-# CATEGORY_BUSINESS = 2
-# CATEGORY_ENTERTAINMENT = 3
-# CATEGORY_TECHNOLOGY = 4
-# CATEGORY_HEALTH = 5
-# CATEGORY_SCIENCE = 6
-# CATEGORY_OTHER = 7
-
-# CATEGORY_CHOICES = (
-#     (CATEGORY_POLITICS, 'politics'),
-#     (CATEGORY_SPORTS, 'sports'),
-#     (CATEGORY_BUSINESS, 'business'),
-#     (CATEGORY_ENTERTAINMENT, 'entertainent'),
-#     (CATEGORY_TECHNOLOGY, 'technology'),
-#     (CATEGORY_HEALTH, 'health'),
-#     (CATEGORY_SCIENCE, 'science'),
-#     (CATEGORY_OTHER, 'other')
-# )
-
 class Topic(models.Model):
 	CATEGORY_POLITICS = 0
 	CATEGORY_SPORTS = 1
--- news_site/api/views.py
+++ news_site/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
+from api.models import ArticleInfo
 
 # Create your views here.
 def index(request):
@@ -10,6 +11,7 @@ def index(request):
 @api_view(['GET'])  # how to load more?
 def get_articles(request):
 	data = {}
+	all_articles = ArticleInfo.objects.all()
+	print(all_articles)
 	# request should have user ID, maybe dates
-	return HttpResponse(data)
-
+	return JSonResponse(data)
