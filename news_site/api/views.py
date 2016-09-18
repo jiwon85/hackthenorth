5a460bf9a9990aef514096812b9ca6336a0f202b
Steven Bairos-Novak <sbairosnovak@gmail.com>
2016-09-17 19:04:20 -0500

    Passing the articles as a list of tuples

--- news_site/api/views.py
+++ news_site/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
-from api.models import ArticleInfo
+from api.models import ArticleInfo, Topic
 from django.core import serializers
 
 # Create your views here.
@@ -11,12 +11,17 @@ def index(request):
 
 @api_view(['GET'])  # how to load more?
 def get_articles(request):
-	data = serializers.serialize('json', ArticleInfo.objects.all())
+
+	topics = []	
+	for topic in Topic.objects.all():
+		topic_list_tuple = (serializers.serialize('json', [topic,]), serializers.serialize('json', ArticleInfo.objects.all()))
+		topics.append(topic_list_tuple)
+
 	# arr = []
 	# for a in ArticleInfo.objects.all():
 	# 	arr.append(a)
 	# print(arr)
 	# request should have user ID, maybe dates
 	
-	return JsonResponse(data, safe=False)
+	return JsonResponse(topics, safe=False)
 
